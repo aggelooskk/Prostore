7b1f1afb834394d39c85bf2e8f082291a5103173
Aggelosk <akermezo@yahoo.com>
2025-07-25 16:54:47 +0300

    Load products from database

---
+++ lib/actions/product.actions.ts
@@ -0,0 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-expressions
+`use server`;
+import { PrismaClient } from "@prisma/client";
+import { convertToPlainObject } from "../utils";
+import { LATEST_PRODUCTS_LIMIT } from "../constants";
+
+// Get latest products
+export async function getLatestProducts() {
+  const prisma = new PrismaClient();
+
+  const data = await prisma.product.findMany({
+    take: LATEST_PRODUCTS_LIMIT,
+    orderBy: { createdAt: `desc` },
+  });
+  return convertToPlainObject(data);
+}
