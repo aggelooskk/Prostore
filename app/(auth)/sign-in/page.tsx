3c5bdef94a887bc506802fa2e37cae943939dcec
Aggelosk <akermezo@yahoo.com>
2025-07-29 11:19:30 +0300

    Auth layout and sign-in page

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,42 @@
+import { Metadata } from "next";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+import Image from "next/image";
+import { APP_NAME } from "@/lib/constants";
+
+export const metadata: Metadata = {
+  title: `Sign In`,
+};
+
+const SignInPage = () => {
+  return (
+    <div className="w-full max-w-md mx-auto">
+      <Card>
+        <CardHeader className="space-y-4">
+          <Link href="/" className="flex-center">
+            <Image
+              src="/images/logo.svg"
+              height={100}
+              width={100}
+              alt={`${APP_NAME} logo`}
+              priority={true}
+            />
+          </Link>
+          <CardTitle className="text-center">Sign In</CardTitle>
+          <CardDescription className="text-center">
+            Sing In To Your Account
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4"></CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default SignInPage;
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,8 @@
+import { Metadata } from "next";
 import ProductList from "@/components/shared/product/product-list";
 import { getLatestProducts } from "@/lib/actions/product.actions";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: `Home`,
 };
 
