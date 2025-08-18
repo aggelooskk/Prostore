c1a3fc0127324bdf5fe7cd21141b6dab7312b331
Aggelosk <akermezo@yahoo.com>
2025-08-18 10:19:42 +0300

    Jest testing for access token

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -3,7 +3,7 @@ const base = process.env.PAYPAL_API_URL || "https://api-m.sandbox.paypal.com";
 export const paypal = {};
 
 // Generate paypal access token
-export async function generateAccessToken() {
+async function generateAccessToken() {
   const { PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET } = process.env;
   const auth = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_APP_SECRET}`).toString(
     "base64"
@@ -18,9 +18,12 @@ export async function generateAccessToken() {
     },
   });
   if (response.ok) {
-    return response.json();
+    const jsonData = await response.json();
+    return jsonData.access.token;
   } else {
     const errorMessage = await response.text();
     throw new Error(errorMessage);
   }
 }
+
+export { generateAccessToken };
