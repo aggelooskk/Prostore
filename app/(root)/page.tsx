ce7d6f909513ca8b46b616dca502d177b5652e7b
Aggelosk <akermezo@yahoo.com>
2025-09-12 16:59:04 +0300

    Featured Products Carousel

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,24 @@
+import ProductCarousel from "@/components/shared/product/product-carousel";
 import ProductList from "@/components/shared/product/product-list";
-import { getLatestProducts } from "@/lib/actions/product.actions";
+import {
+  getFeaturedProducts,
+  getLatestProducts,
+} from "@/lib/actions/product.actions";
+import { Metadata } from "next";
 
-export const metadata={
+export const metadata: Metadata = {
   title: `Home`,
 };
 
 const Homepage = async () => {
   const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
 
   return (
     <>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
       <ProductList data={latestProducts} title="Newest Arrivals" />
     </>
   );
